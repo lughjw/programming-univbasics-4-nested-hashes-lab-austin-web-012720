be97cb9da6e98f0a10138d76d704f5d0273577d5
Lugh Wilson <lughjw@gmail.com>
2019-12-27 04:09:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -43,21 +43,21 @@ end
 
 def dennis_ritchies_language
 	programmer_hash = 
- 	{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-   }
-
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+  
   programmer_hash[dennis_ritchie][:languages]
 end
 
@@ -106,7 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
